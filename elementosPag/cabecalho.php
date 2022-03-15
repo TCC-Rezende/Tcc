6a19c980dd92e675f9c8d08967c0a716d7626f13
TCC-Rezende <95300060+TCC-Rezende@users.noreply.github.com>
2022-03-15 12:38:29 -0300

    Add files via upload

---
+++ elementosPag/cabecalho.php
@@ -0,0 +1,12 @@
+    <header class="header">
+
+        <a href="#"><img src="imagens/LogoSample_ByTailorBrands.jpg" alt="*" ></a>
+        
+        <nav>
+          <ul class="menu">
+            <li><a href="#">Coleção</a></li>
+            <li><a href="#">Carrinho</a></li>
+            <li><a href="#">Login</a></li> 
+          </ul>
+        </nav>
+    </header>
