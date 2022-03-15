b4c8bfcf135d0932040f99062b19170477d0c4bb
TCC-Rezende <95300060+TCC-Rezende@users.noreply.github.com>
2022-03-15 12:40:58 -0300

    arquivo inicial

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <title>Título</title>
+    <!--
+     <link rel="stylesheet" type="text/css" href="estilos/default.css">
+    <link rel="stylesheet" type="text/css" href="estilos/cabecalho.css">
+    <link rel="stylesheet" type="text/css" href="estilos/styleInicio.css"> -->
+    <style>
+    <?php
+    include_once "estilos/padrao.css";
+    include_once "estilos/cabecalho.css";
+    include_once "estilos/styleInicio.css";
+    ?>
+    </style>
+</head>
+
+<body>
+    <!-- Cabeçalho da página  -->
+    <?php
+    include_once "elemantosPag/cabecalho.php";
+    ?>
+    <!-- Corpo da página  -->
+    <div class="container">
+    <div class="titulo">
+    <h1>Bem-Vindo</h1>
+    </div>
+    </div>
+</body>
+</html>
