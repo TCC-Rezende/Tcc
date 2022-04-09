078ea408c4390224610bf5e989e7c31f43a834bd
Gabriel-Mazeto <96828628+Gabriel-Mazeto@users.noreply.github.com>
2022-04-09 14:30:52 -0300

    alteração e commit no dia 09/04

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    
      <title>Título</title>
     <!--
      <link rel="stylesheet" type="text/css" href="estilos/default.css">
@@ -12,9 +12,11 @@
     <link rel="stylesheet" type="text/css" href="estilos/styleInicio.css"> -->
     <style>
     <?php
+    // estilos necessários
     include_once "estilos/padrao.css";
     include_once "estilos/cabecalho.css";
     include_once "estilos/rodape.css";
+    // estilos da página
     include_once "estilos/styleInicio.css";
     ?>
     </style>
@@ -29,14 +31,19 @@
     <h1>Bem-Vindo</h1>
     </div>
     
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae asperiores, corrupti autem impedit culpa voluptatem unde aspernatur hic cupiditate, odio fugiat. Laudantium et inventore reprehenderit, impedit consequatur molestiae commodi voluptatum magnam veritatis dolorum ratione at quidem quis tenetur, nulla animi doloribus unde nostrum optio eveniet voluptatibus, molestias officiis. Omnis quos explicabo quia vero recusandae ea, nesciunt dolores. Ipsam id pariatur quae, voluptatum cupiditate porro reprehenderit minima corrupti. Aliquid ducimus minus inventore aperiam deserunt, odit eum accusamus. Quae ad dignissimos nemo et voluptatem velit, ipsam quas, rerum, impedit modi fugit ut distinctio ab quia sapiente natus illo aliquam eos. Fugit veniam saepe et dolorem. Placeat, dolor.<br>
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati maxime numquam labore facere quos earum harum alias voluptatum, ex veritatis aspernatur? Aspernatur laborum doloribus quibusdam iste possimus mollitia sint non reprehenderit, eius ullam dolore deleniti, ut sit harum consequatur exercitationem voluptates alias nam. Eligendi obcaecati optio necessitatibus debitis dolorem dolorum, quod, facilis laborum ab quidem reiciendis, nihil at illum voluptatum repellendus aliquam quis animi doloribus earum incidunt cumque nemo! Aspernatur doloremque quibusdam error sapiente iure, ex saepe aliquid illo laudantium sed harum explicabo, debitis fugit provident ipsa natus. Optio facilis at, necessitatibus cum dolor nesciunt quo aliquam minima enim eligendi assumenda labore aspernatur praesentium provident. Quis quam culpa ex unde facere odio quibusdam, alias consequatur perspiciatis sit explicabo ad earum voluptas dolores deleniti maxime temporibus quia eius quidem porro qui hic eum! Dicta ipsa ratione quisquam recusandae optio nihil illum architecto a quod. Iure quisquam hic pariatur soluta officia, nostrum iste dolores vero repellat. Nihil accusamus delectus ea harum libero inventore tempore quia? Voluptate laudantium tenetur vel soluta omnis, libero laborum cum rem beatae ut tempora saepe nihil nemo, voluptas, quo odio et nisi quos illum dolorum expedita necessitatibus? Veniam facere mollitia, quisquam voluptas tempora voluptatibus at sint autem maiores!
+</p>
 
 <?php include_once "elementosPag/rodape.html";      ?>
 </div>
 
 
 <!-- Links para os icones & logos usados -->
+
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
 <!-- ********************************* -->
 
 </body>
