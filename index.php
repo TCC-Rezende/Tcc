066458331b1b2704196919d7c04639454e6f842d
Gabriel-Mazeto <96828628+Gabriel-Mazeto@users.noreply.github.com>
2022-04-12 14:42:09 -0300

    alterações 12/04

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5; ../index.php  ">
+
     
      <title>Título</title>
     <!--
-     <link rel="stylesheet" type="text/css" href="estilos/default.css">
+    <link rel="stylesheet" type="text/css" href="estilos/default.css">
     <link rel="stylesheet" type="text/css" href="estilos/cabecalho.css">
     <link rel="stylesheet" type="text/css" href="estilos/styleInicio.css"> -->
     <style>
