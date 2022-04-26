141016b79a266654fd053780a3a03aacd0c1fdf7
Gabriel-Mazeto <96828628+Gabriel-Mazeto@users.noreply.github.com>
2022-04-26 14:40:32 -0300

    Aula 26-04

--- action/cabecalho.js
+++ action/cabecalho.js
@@ -16,13 +16,19 @@ function pesqAtiva(){
 
 function  menuToggle(){
     let menuTG = document.querySelector(".menu .toggle button")
+    let container = document.querySelector(".menu.nav-container")
+    let body = document.querySelector("body")
     let valida = menuTG.classList.value
    
-   
+    
     if (valida === "ativo"){
         menuTG.classList.remove("ativo")
+        container.classList.remove("ativo")
+        body.classList.remove("ativo")
     }else{
         menuTG.classList.add("ativo")
+        container.classList.add("ativo")
+        body.classList.add("ativo")
     }
 
 }
