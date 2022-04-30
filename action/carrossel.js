e0a5b95450e9f11da1cb4b964dfbf8b3c246195a
Gabriel-Mazeto <96828628+Gabriel-Mazeto@users.noreply.github.com>
2022-04-30 15:10:09 -0300

    alterações dia 30/04

--- action/carrossel.js
+++ action/carrossel.js
@@ -1,17 +1,29 @@
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
+let body = document.querySelector("body")
+let containerImgs = document.querySelector("#img");
+let imagens = document.querySelectorAll("#img img");
+// imagens[0] -> camisas
+// imagens[1] -> neymar
+// imagens[2] -> salah
 
-let idx= 0;
 
-function carrossel (){
-    idx++;
+function carrossel(){
+    imagens[0].classList.add('visivel')
 
-    if(idx > img.length-1){
-        idx= 0;
+    if (imagens[0].classList.value == 'visivel'){
+        imagens[0].style.right = 0 
+        imagens[0].style.trasition = 0.5+"s" 
+
+        imagens[0].classList.remove('visivel')
+        imagens[1].classList.add('visivel')
+
+    }else if(imagens[1].classList.value == 'visivel'){
+        imagens[0].style.right = 100+'%' 
+        
+        imagens[1].style.right = 0 
+        imagens[1].style.trasition = 0.5+"s" 
+
+        imagens[1].classList.remove('visivel')
+
     }
- 
-    imgs.style.transform= `translateX(${-idx * 500}px)`;
-
-   
+    
 }
-setInterval(carrossel, 1800);
--- index.php
+++ index.php
@@ -11,14 +11,14 @@
     <link rel="stylesheet" type="text/css" href="estilos/padrao.css">
     <link rel="stylesheet" type="text/css" href="estilos/cabecalho.css">
     <link rel="stylesheet" type="text/css" href="estilos/rodape.css">   
+    
+    <!-- //Estilos da pagina  -->
+    
     <link rel="stylesheet" type="text/css" href="estilos/carrossel.css">   
-    
-<!-- //Estilos necessários  -->
-    
     <link rel="stylesheet" type="text/css" href="estilos/styleInicio.css"> 
 </head>
 
-<body>
+<body onload="carrossel()">
     <!-- Cabeçalho da página  -->
 <div class="container">
     <?php  include_once "elementos/cabecalho.html";   ?>
@@ -27,17 +27,13 @@
     <h1>Bem-Vindo</h1>
     </div>
         <div class="carrossel">
-            <div class="container_car" id="img">
+            <div class="container-car" id="img">
                 <img src="imagens/camisa.png" alt="">
                 <img src="imagens/neymar.png" alt="">
                 <img src="imagens/salah.png" alt="">
            
             </div>
-
         </div>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae asperiores, corrupti autem impedit culpa voluptatem unde aspernatur hic cupiditate, odio fugiat. Laudantium et inventore reprehenderit, impedit consequatur molestiae commodi voluptatum magnam veritatis dolorum ratione at quidem quis tenetur, nulla animi doloribus unde nostrum optio eveniet voluptatibus, molestias officiis. Omnis quos explicabo quia vero recusandae ea, nesciunt dolores. Ipsam id pariatur quae, voluptatum cupiditate porro reprehenderit minima corrupti. Aliquid ducimus minus inventore aperiam deserunt, odit eum accusamus. Quae ad dignissimos nemo et voluptatem velit, ipsam quas, rerum, impedit modi fugit ut distinctio ab quia sapiente natus illo aliquam eos. Fugit veniam saepe et dolorem. Placeat, dolor.<br>
-Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati maxime numquam labore facere quos earum harum alias voluptatum, ex veritatis aspernatur? Aspernatur laborum doloribus quibusdam iste possimus mollitia sint non reprehenderit, eius ullam dolore deleniti, ut sit harum consequatur exercitationem voluptates alias nam. Eligendi obcaecati optio necessitatibus debitis dolorem dolorum, quod, facilis laborum ab quidem reiciendis, nihil at illum voluptatum repellendus aliquam quis animi doloribus earum incidunt cumque nemo! Aspernatur doloremque quibusdam error sapiente iure, ex saepe aliquid illo laudantium sed harum explicabo, debitis fugit provident ipsa natus. Optio facilis at, necessitatibus cum dolor nesciunt quo aliquam minima enim eligendi assumenda labore aspernatur praesentium provident. Quis quam culpa ex unde facere odio quibusdam, alias consequatur perspiciatis sit explicabo ad earum voluptas dolores deleniti maxime temporibus quia eius quidem porro qui hic eum! Dicta ipsa ratione quisquam recusandae optio nihil illum architecto a quod. Iure quisquam hic pariatur soluta officia, nostrum iste dolores vero repellat. Nihil accusamus delectus ea harum libero inventore tempore quia? Voluptate laudantium tenetur vel soluta omnis, libero laborum cum rem beatae ut tempora saepe nihil nemo, voluptas, quo odio et nisi quos illum dolorum expedita necessitatibus? Veniam facere mollitia, quisquam voluptas tempora voluptatibus at sint autem maiores!
-</p>
 
 <?php include_once "elementos/rodape.html";      ?>
 </div>
